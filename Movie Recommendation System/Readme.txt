c1200a12468352c5b3cfd75393b39df33b13d7a5
rishabh <rishabh.2125cse1042@kiet.edu>
2024-06-01 09:37:48 +0530

    commited files Git lfs

---
+++ Movie Recommendation System/Readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8fb44f4ac626f9b49a16983f7ac58766f98e1cace6132e83bc1fd22cb510d3f3
+size 146
---
+++ Movie Recommendation System/streamlitapp.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c072f9893631516bec6c312a3a237a55508192e70d33bccb9fe61f3d815a2ff
+size 3080
